0fe834953ef81c7c3ac6c832f89ca9dc0db820d9
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2025-07-25 15:21:08 +0000

    created first api i.e models .py

---
+++ api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    pages = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
